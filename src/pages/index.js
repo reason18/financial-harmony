bfc2371895bc37cae5dbd2016a8b9e3f74982872
misha <misha.baturin96@gmail.com>
2019-05-19 18:57:43 +0300

    added fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 
 import { Layout, Image, SEO } from "../components";
 import { Promo, OutServices, AboutUs } from "../components/_sections";
+import "../assets/fonts/fonts.css";
 
 import "./index.scss";
 
